af9c840bc9f850eff274ddd8c6379757eac29def
Fahim <fahimsafaat67@gmail.com>
2025-05-03 21:49:45 +0600

    list view grid view

--- lib/Module_08/Class_02/gridviewhome.dart
+++ lib/Module_08/Class_02/gridviewhome.dart
@@ -18,86 +18,86 @@ class Gridviewhome extends StatelessWidget {
 
       //gridview. count
 
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(10),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        children: List.generate(10, (index) {
-          return Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const Icon(Icons.phone, color: Colors.blue),
-                      const Spacer(),
-                      const Icon(Icons.delete, color: Colors.red),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    "Taufiq",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  const Text("017929562"),
-                ],
-              ),
-            ),
-          );
-        }),
-      ),
-
-
-      // //gridview.builder
-      // body: GridView.builder(
-      //
-      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //
-      //   crossAxisCount: 3,//how many will be shown
-      //   crossAxisSpacing: 5,
-      //   mainAxisSpacing: 5,
-      //
-      //   ),
-      //
-      // itemCount: 20,//total item
-      //   itemBuilder: (context, index){
-      //
-      //     return Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //
-      //       child: Card(
-      //         elevation: 10,
-      //
-      //         //main part
-      //         child: Container(
-      //           decoration: BoxDecoration(
-      //             color: Colors.orange,
-      //             borderRadius: BorderRadius.circular(12),
-      //           ),
-      //
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   padding: const EdgeInsets.all(10),
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      //   children: List.generate(10, (index) {
+      //     return Card(
+      //       elevation: 5,
+      //       shape: RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.circular(12),
+      //       ),
+      //       child: Padding(
+      //         padding: const EdgeInsets.all(12),
       //         child: Column(
-      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           crossAxisAlignment: CrossAxisAlignment.start,
       //           children: [
-      //             Icon(Icons.phone, color: Colors.white, size: 25,),
-      //             SizedBox(height: 10,),
-      //             Text("Send Money", style: TextStyle(color: Colors.white, fontSize: 15),),
+      //             Row(
+      //               children: [
+      //                 const Icon(Icons.phone, color: Colors.blue),
+      //                 const Spacer(),
+      //                 const Icon(Icons.delete, color: Colors.red),
+      //               ],
+      //             ),
+      //             const SizedBox(height: 16),
+      //             const Text(
+      //               "Taufiq",
+      //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      //             ),
+      //             const Text("017929562"),
       //           ],
-      //
-      //         ),
-      //
       //         ),
       //       ),
       //     );
-      //
-      //   },
+      //   }),
       // ),
+
+
+      // //gridview.builder
+      body: GridView.builder(
+
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+
+        crossAxisCount: 3,//how many will be shown
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+
+        ),
+
+      itemCount: 20,//total item
+        itemBuilder: (context, index){
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+
+            child: Card(
+              elevation: 10,
+
+              //main part
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone, color: Colors.white, size: 25,),
+                  SizedBox(height: 10,),
+                  Text("Send Money", style: TextStyle(color: Colors.white, fontSize: 15),),
+                ],
+
+              ),
+
+              ),
+            ),
+          );
+
+        },
+      ),
 
     );
 
