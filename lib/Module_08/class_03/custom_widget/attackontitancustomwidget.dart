a9982d35cfe48a8bc24819dc35485b393dcb2306
Fahim <fahimsafaat67@gmail.com>
2025-05-06 22:59:42 +0600

    gif added

--- lib/Module_08/class_03/custom_widget/attackontitancustomwidget.dart
+++ lib/Module_08/class_03/custom_widget/attackontitancustomwidget.dart
@@ -1,65 +1,94 @@
+//card modification
+//   Card(
+//     //card styles
+//       color: Colors.black,
+//       elevation: 10,
+//       //shadowColor: Colors.redAccent,
+//       shape: RoundedRectangleBorder(
+//
+//         //container all side box shape modify
+//         borderRadius: BorderRadius.only(
+//           //bottomLeft: Radius.circular(20),
+//           bottomRight: Radius.circular(30),
+//           topLeft: Radius.circular(30),
+//           //topRight: Radius.circular(40),
+//
+//         ),
+//
+//         //border colors
+//         side: BorderSide(
+//           color: Colors.redAccent,
+//           width: 3,
+//         ),
+//       ),
+//   ),
+
 import 'package:flutter/material.dart';
 
 class Attackontitan extends StatelessWidget {
-  final String titangif, titanname;
+  final String titangif, titanname, titanrate;
   const Attackontitan({
-    super.key, required this.titangif, required this.titanname,
+    super.key, required this.titangif, required this.titanname, required this.titanrate,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      //card styles
-      color: Colors.black,
-      elevation: 10,
-      //shadowColor: Colors.redAccent,
-      shape: RoundedRectangleBorder(
+    return Container(
+      margin: EdgeInsets.only(top: 30, left: 15, right: 15, bottom: 10),
+      padding: EdgeInsets.only(top: 30, left: 15, right: 15, bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(4, 4),
+            blurRadius: 5,
+          ),
 
-        //container all side box shape modify
-        borderRadius: BorderRadius.only(
-          //bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(30),
-          topLeft: Radius.circular(30),
-          //topRight: Radius.circular(40),
+        ],
 
-        ),
-
-        //border colors
-        side: BorderSide(
-          color: Colors.redAccent,
-          width: 3,
-        ),
       ),
 
+      width: 350,
+      child: Column(
+            children: [
+            Stack(
+              children: [
+                Image.network(titangif,),
+                //positioned
+                Positioned(
+                  bottom: 10,
+                    left: 5,
 
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(10, 10),
-              blurRadius: 15,
+                    child: Container(
+                      width: 60,
+                  color: Colors.black,
+                      child: Row(
+                        children: [
+                          Icon(Icons.star, size: 20, color: Colors.yellow,),
+
+                          Text(titanrate, style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+
+                ))
+              ],
             ),
 
-          ],
 
-        ),
-
-        width: 350,
-        child: Column(
-              children: [
-              Image.network(
-                titangif,
-              ),
-
-
-            Text(titanname, style: TextStyle(
-              fontSize: 30,
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-            )),
-          ],
-        )
-        ),
+          Text(titanname, style: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          )),
+        ],
+      )
       );
   }
 }
--- lib/Module_08/class_03/statefull/statefullhome.dart
+++ lib/Module_08/class_03/statefull/statefullhome.dart
@@ -48,7 +48,8 @@ class _counterwithstatefullwidgetState
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Titan Killed:\n$counter".toString(),
+            Text(
+              "Titan Killed:\n$counter".toString(),
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 50,
@@ -57,101 +58,152 @@ class _counterwithstatefullwidgetState
               ),
             ),
 
-            SizedBox(height: 50),
+            SizedBox(height: 10),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: 150,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
+                Column(
+                  children: [
+                      Row(
+                        children: [
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
 
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              //refreshed the page and showed in app only works in stful
+                              counter--;
+                              print("Counter now: $counter");
+                            });
+
+                            // //counter up in terminal
+                            // counter--;
+                            // print("Counter now: $counter");
+                          },
+                          child: Text(
+                            "-",
+                            style: TextStyle(fontSize: 50, color: Colors.black),
+                          ),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        //refreshed the page and showed in app only works in stful
-                        counter--;
-                        print("Counter now: $counter");
-                      });
 
-                      // //counter up in terminal
-                      // counter--;
-                      // print("Counter now: $counter");
-                    },
-                    child: Text(
-                      "-",
-                      style: TextStyle(fontSize: 50, color: Colors.black),
-                    ),
-                  ),
-                ),
+                      SizedBox(width: 20),
 
-                SizedBox(width: 20),
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
 
-                SizedBox(
-                  width: 150,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              //refreshed the page and showed in app only works in stful
+                              counter++;
+                              print("Counter now: $counter");
+                            });
 
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
+                            // //counter up in terminal
+                            // counter++;
+                            // print("Counter now: $counter");
+                          },
+                          child: Text(
+                            "+",
+                            style: TextStyle(
+                              fontSize: 50,
+                              color: Colors.lightGreenAccent,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        //refreshed the page and showed in app only works in stful
-                        counter++;
-                        print("Counter now: $counter");
-                      });
-
-                      // //counter up in terminal
-                      // counter++;
-                      // print("Counter now: $counter");
-                    },
-                    child: Text(
-                      "+",
-                      style: TextStyle(
-                        fontSize: 50,
-                        color: Colors.lightGreenAccent,
+                        ],
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 80,
+                              width: 180,
+                              child: Image.network(
+                                "https://gifdb.com/images/high/armin-498-x-278-gif-jitkq870p7ym6g16.webp",
+                              ),
+                            ),
+                            SizedBox(
+                              height: 80,
+                              width: 150,
+                              child: Image.network(
+                                "https://images.steamusercontent.com/ugc/1047596647999422866/F476F35240D7FDC527CF237D47142A3BFCA6F970/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+                              ),
+                            ),
+                          ],
+
+                        ),
+                      ),
+                  ],
                     ),
-                  ),
-                ),
               ],
             ),
 
-            SizedBox(height: 50),
+            SizedBox(height: 5),
 
             //customwidget called in a row
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  SizedBox(width: 20,),
-                  Attackontitan(titangif: 'https://images.steamusercontent.com/ugc/823440303646988829/057D4CB42766804CD18CC5E9850FCC6B2C9B9388/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false', titanname: 'Colossal titan',),
-                  SizedBox(width: 20,),
-                   Attackontitan(titangif: 'https://giffiles.alphacoders.com/115/115281.gif', titanname: 'Beast titan',),
-                   SizedBox(width: 20,),
-                   Attackontitan(titangif: 'https://64.media.tumblr.com/5fd6d72ba457b051d1ad00fcd8a1b0f1/tumblr_n60c6ouoGZ1raxepao1_500.gifv', titanname: 'Female titan',),
-                   SizedBox(width: 20,),
-                   Attackontitan(titangif: 'https://64.media.tumblr.com/ed1f2369893ced035d996b93d07e6f9c/tumblr_n60c6ouoGZ1raxepao3_500.gifv', titanname: 'Attack titan'),
-                    SizedBox(width: 20,),
-                  Attackontitan(titangif: 'https://giffiles.alphacoders.com/219/219658.gif', titanname: 'Armored titan',),
+                  SizedBox(width: 20),
+                  Attackontitan(
+                    titangif:
+                        'https://images.steamusercontent.com/ugc/823440303646988829/057D4CB42766804CD18CC5E9850FCC6B2C9B9388/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                    titanname: 'Colossal titan',
+                    titanrate: '9.5',
+                  ),
+                  SizedBox(width: 20),
+                  Attackontitan(
+                    titangif: 'https://giffiles.alphacoders.com/115/115281.gif',
+                    titanname: 'Beast titan',
+                    titanrate: '9.2',
+                  ),
+                  SizedBox(width: 20),
+                  Attackontitan(
+                    titangif:
+                        'https://64.media.tumblr.com/5fd6d72ba457b051d1ad00fcd8a1b0f1/tumblr_n60c6ouoGZ1raxepao1_500.gifv',
+                    titanname: 'Female titan',
+                    titanrate: '9.7',
+                  ),
+                  SizedBox(width: 20),
+                  Attackontitan(
+                    titangif:
+                        'https://64.media.tumblr.com/ed1f2369893ced035d996b93d07e6f9c/tumblr_n60c6ouoGZ1raxepao3_500.gifv',
+                    titanname: 'Attack titan',
+                    titanrate: '10',
+                  ),
+                  SizedBox(width: 20),
+                  Attackontitan(
+                    titangif:
+                        'https://media1.tenor.com/m/YdWxpsrTXccAAAAd/armored-titan-attack-on-titan.gif',
+                    titanname: 'Armored titan',
+                    titanrate: '8.5',
+                  ),
                 ],
-              
               ),
-            )
-
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
