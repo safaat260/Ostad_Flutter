f47a03148ff6a30d5071e9033bf0bd228640a988
Fahim <fahimsafaat67@gmail.com>
2025-05-08 23:34:52 +0600

    1

---
+++ lib/Module_09/class 1/mediaqueryapp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:test1/Module_09/class%201/mediaqueryhome.dart';
+
+class mediaqueryapp extends StatefulWidget {
+  const mediaqueryapp({super.key});
+
+  @override
+  State<mediaqueryapp> createState() => _mediaqueryappState();
+}
+
+class _mediaqueryappState extends State<mediaqueryapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "media query",
+      home: mediaqueryhome(),
+    );
+  }
+}
---
+++ lib/Module_09/class 1/mediaqueryhome.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class mediaqueryhome extends StatefulWidget {
+  const mediaqueryhome({super.key});
+
+  @override
+  State<mediaqueryhome> createState() => _mediaqueryhomeState();
+}
+
+class _mediaqueryhomeState extends State<mediaqueryhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+
+      ),
+
+    );
+  }
+}
