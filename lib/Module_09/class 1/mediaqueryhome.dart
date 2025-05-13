3bcf74234644af19415751aa5ef21b03aa116a27
Fahim <fahimsafaat67@gmail.com>
2025-05-14 00:20:06 +0600

    wrap & chip

--- lib/Module_09/class 1/mediaqueryhome.dart
+++ lib/Module_09/class 1/mediaqueryhome.dart
@@ -10,7 +10,15 @@ class mediaqueryhome extends StatefulWidget {
 class _mediaqueryhomeState extends State<mediaqueryhome> {
   @override
   Widget build(BuildContext context) {
+    //screensize
     Size screensize = MediaQuery.of(context).size;
+
+    //fontsize
+    double font_width_condtion = screensize.width > 700 ? 60 :20;
+
+    //pixelratio
+    double pixelratio = MediaQuery.of(context).devicePixelRatio;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Media Query", style: TextStyle(
@@ -36,10 +44,30 @@ class _mediaqueryhomeState extends State<mediaqueryhome> {
               height: screensize.height*0.5,
                 color: Colors.red,
               ),
-            )
+            ),
+
+            //font
+            Text("MediaQuery Font check!", style: TextStyle(
+              fontSize: font_width_condtion,
+            ),),
+
+            //pixelration
+            Text("Pixel Ratio: $pixelratio"),
+
+            //wrap
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 4.0,
+              children:
+                List.generate(30, (index){
+                  return Chip(label: Text("item $index"));
+                }),
+            ),
+
           ],
         ),
       ),
+
 
 
 
