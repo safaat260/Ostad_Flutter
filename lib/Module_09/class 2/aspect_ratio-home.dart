821ad7c2ae601c84bc79b96cdce1d26c3196a936
Fahim <fahimsafaat67@gmail.com>
2025-05-15 16:57:50 +0600

    fractionally sizedbox

--- lib/Module_09/class 2/aspect_ratio-home.dart
+++ lib/Module_09/class 2/aspect_ratio-home.dart
@@ -20,18 +20,48 @@ class _aspect_homeState extends State<aspect_home> {
             color: Colors.white,
           ),
         ),
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.cyan,
       ),
 
       //aspect ratio
       body: SingleChildScrollView(
         child: Column(
           children: [
+
+            //fractionally sized box
+            FractionallySizedBox(
+              //heightFactor: 0.1,
+              widthFactor: 0.5,//takes 50% of the space
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Submit")),
+            ),
+
+            SizedBox(height: 4),
+
+            Container(
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 3),
+                color: Colors.yellow
+              ),
+              height: 300,
+              child: FractionallySizedBox(
+                heightFactor: 0.5,// took 50% of 400 height
+                child: Container(
+                  color: Colors.grey,
+                  child: Center(child: Text("center text", style: TextStyle(fontSize: 20),)),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 4),
+
             //felxible and flex
             Row(
               children: [
                 Flexible(
-                  flex: 3,//flex
+                  flex: 3, //flex
                   //fit: FlexFit.tight/loose, //fit
                   child: Container(height: 100, color: Colors.black),
                 ),
@@ -65,7 +95,7 @@ class _aspect_homeState extends State<aspect_home> {
             Row(
               children: [
                 Expanded(
-                  flex: 2,//flex
+                  flex: 2, //flex
                   child: ElevatedButton(
                     onPressed: () {},
                     child: Text("Person-1"),
@@ -85,7 +115,6 @@ class _aspect_homeState extends State<aspect_home> {
                     child: Text("Person-2"),
                   ),
                 ),
-
               ],
             ),
 
